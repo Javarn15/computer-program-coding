1870a8704f04729039b6c52c42b556816d5b5a95
javarn <javarnay10@gmail.com>
2025-04-30 10:44:28 +1200

    added a variable to validate you have to be 5 to 17 to be able to go to camp

--- program.py
+++ program.py
@@ -1,2 +1,10 @@
 name = input("What is your name")
 age = input("what is your age")
+
+
+def validate_age(age):
+    if age < 4:
+        print("Sorry, you must be 5 or older to participate.")
+        exit()
+    if age > 18:
+        print("Sorry, you must be 17 or younger to go to camp .")
